e0134dc15dba8cb96b943107a40d4f0ab01c52b0
hyobob1210 <pop30258025@gmail.com>
2024-10-08 15:58:36 +0900

    Add files via upload

---
+++ 035_p 150.4.cpp
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int a[10][10];
+
+    int x, y;
+
+    printf("행과 열의 숫자를 입력 : ");
+    scanf_s("%d %d", &x, &y);
+
+    
+    // 각 행의 합을 구하여 출력하라
+    printf("각 행의 합을 출력 :\n");
+    
+    for (int i = 0; i < x; i++) {
+        int sum = 0;
+        for (int j = 0; j < y; j++)
+            sum += a[i][j];
+            printf("%4d\n", sum);
+       
+}
